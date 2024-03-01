f1db15ce7d991e0f830ff64dc89982fcf133d0cd
Ravikanth-rk <pedapatiravikanth@gmail.com>
2024-03-01 10:58:08 +0530

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        "id":1,
+        "name": "Ravikanth",
+        "Age":25
+    },
+    {
+        "id":2,
+        "name": "kohli",
+        "Age":35
+    },
+    {
+        "id":3,
+        "name": "Ramcharan",
+        "Age":34
+    }
+]
+
+export default Employees;
